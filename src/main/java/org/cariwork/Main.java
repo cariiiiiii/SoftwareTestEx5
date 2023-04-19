7dfbc0164dec8b492b66102b74d41b6ec96db886
cariiiiiii <906491726@qq.com>
2023-04-19 19:08:56 +0800

    second modify

--- src/main/java/org/cariwork/Main.java
+++
@@ -1,7 +0,0 @@
-package org.cariwork;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- src/test/java/mimic/LinkedListTest.java
+++ src/test/java/mimic/LinkedListTest.java
@@ -1,22 +1,38 @@
 package mimic;
 
-
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LinkedListTest {
+
+
     @Test
-    void should_remove_first_null_element_when_remove_null(){
-        List<String> list = new LinkedList<>();
-        list.add(null);
-        list.add(null);
+    public void linkedListTest() {
 
-        list.remove(null);
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
 
-        assertEquals(1,list.size());
+
+        assertEquals(1, list.remove().intValue());
+        assertEquals(2, list.size());
+
+        assertTrue(list.removeLastOccurrence(3));
+        assertEquals(1, list.size());
+        assertEquals(2, list.get(0).intValue());
+
+        assertTrue(list.remove(Integer.valueOf(2)));
+        System.out.println("After remove: " + list);
+        assertFalse(list.contains(2), "List should not contain element 2 after remove operation");
+
+
+        assertFalse(list.remove(Integer.valueOf(10)));
+
+        list.clear();
+        assertThrows(NoSuchElementException.class, () -> list.remove());
     }
 }
